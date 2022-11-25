5cec38afd796d1a3e629d1d3c064ec4b22001eb3
Lahiru <lahiruamarathunga526@gmail.com>
2022-11-25 23:35:42 +0530

    Added some useful links in readme file

--- App.js
+++ App.js
@@ -24,11 +24,6 @@ const App = () => {
   );
 };
 
-// https://community.esri.com/t5/arcgis-runtime-sdk-for-net-questions/exception-details-dlopen-failed-library-quot/m-p/1199040#M11208
-// https://github.com/Esri/arcgis-runtime-samples-android/issues/268
-// https://community.esri.com/t5/arcgis-runtime-sdk-for-android-questions/migrating-from-10-2-8-1-to-100-4-0-in-android/m-p/405111#M2751
-// https://www.google.com/search?q=Error+inflating+class+com.esri.arcgisruntime.mapping.view.MapView&oq=Error+inflating+class+com.esri.arcgisruntime.mapping.view.MapView&aqs=chrome..69i57j69i61.297j0j9&sourceid=chrome&ie=UTF-8
-
 const styles = StyleSheet.create({
   container: {
     flex: 1
