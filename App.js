fc0a565c81df5a2d2fa606acf4ba35e8ad07896a
Lahiru <lahiruamarathunga526@gmail.com>
2022-11-26 18:31:00 +0530

    Added the base map URL

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
 import React, { useRef } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text
-} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import ArcGISMapView from 'react-native-arcgis-mapview'
 
 const App = () => {
+  //initial region for testing purpose
   const initialRegion = [{ latitude: 34.055561, longitude: -117.182602 }]
-
+  // base map URL
+  const mapUrl = "https://services.gisqatar.org.qa/server/rest/services/Vector/Qatar_StreetMap_Hybrid_E/MapServer?f=jsapi"
+  // referrence for accessing DOM elements
   const mapView = useRef();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ArcGISMapView ref={mapView} style={styles.mapStyle}
-        initialMapCenter={initialRegion}
+    <View style={styles.container}>
+      <ArcGISMapView
+        ref={mapView}
+        style={styles.mapStyle}
+        //initialMapCenter={initialRegion} // Disabled the initial region
         recenterIfGraphicTapped={true}
         rotationEnabled={false}
         mapBasemap={{ type: 'normal' }}
+        basemapUrl={mapUrl}
       />
-      <Text>Hello</Text>
-    </SafeAreaView>
+    </View>
   );
 };
 
